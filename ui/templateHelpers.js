56afa3b1e45696df7aabcb08be97f50e38ec64ab
Thomas Wilburn <twilburn@npr.org>
2019-04-10 14:32:21 -0500

    Add template-from-file helper

--- ui/templateHelpers.js
+++ ui/templateHelpers.js
@@ -5,3 +5,14 @@
 function include(filename) {
   return HtmlService.createHtmlOutputFromFile(filename).getContent();
 }
+
+// use a file to generate text output
+// delimiters are the same as the regular Apps Script templates:
+// <? JS code ?>
+// <?= JS output ?>
+// <?!= unsanitized JS output ?>
+function template(filename, data) {
+  var template = HtmlService.createTemplateFromFile(filename);
+  template.data = data;
+  return template.evaluate().getContent().trim();
+}
